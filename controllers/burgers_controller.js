0aaaa9696037d3bc4ce44bf62493b3c2e66cc534
Max Yeamans <max.yeamans@gmail.com>
2018-08-06 06:59:00 -0700

    All necessary files created (though not necessarily working yet).

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,26 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 
 const burger = require("../models/burger.js");
+
+router.get("/", (req, res) => {
+    burger.all( data => {
+        const handlebarObj = {
+            burgers: data
+        };
+        console.log("This burger's info:", handlebarObj);
+        res.render(handlebarObj);
+    });
+});
+
+router.post("/api/burgers", (req, res) => {
+    burger.create(/* TODO: fill this in when views are completed */);
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    const burgerID = req.params.id;
+
+    burger.update(/* TODO: fill this in when views are completed */)
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.port || 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
